277e59085368e3140c65d82b0e9e737381fae1e9
Nitin Bansode <nitinabansode@gmail.com>
2024-06-01 08:43:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import { FaSearch } from "react-icons/fa";
+import { FcSpeaker } from "react-icons/fc";
 
 function App() {
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+  function getMeaning() {
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`)
+      .then((response) => {
+        setData(response.data[0]);
+      });
+  }
+
+  // function to play and listen the
+  // phonetics of the searched word
+
+  function playAudio() {
+    let audio = new Audio(data.phonetics[0].audio);
+    audio.play();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Free Dictionary</h1>
+      <div className="searchBox">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchWord(e.target.value)}
+        />
+        <button onClick={() => getMeaning()}>
+          <FaSearch size="20px" />
+        </button>
+      </div>
+      {data && (
+        <div className="showResults">
+          <h2>
+            {data.word}{" "}
+            <button
+              onClick={() => {
+                playAudio();
+              }}
+            >
+              <FcSpeaker size="26px" />
+            </button>
+          </h2>
+          <h4>Parts of speech:</h4>
+
+          <p>{data.meanings[0].partOfSpeech}</p>
+          <h4>Definitions:</h4>
+          <p>
+            {data.meanings[0].definitions.map((item, index) => (
+              <p key={index}>
+                <b>{index}.</b>
+                {item.definition}
+              </p>
+            ))}
+          </p>
+          <h4>Example:</h4>
+          <p>{data.meanings[0].definitions[0].example}</p>
+        </div>
+      )}
     </div>
   );
 }
